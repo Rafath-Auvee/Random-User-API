9c575491e8a96b744ec785dffcfe1b261bee4101
rafath-bin-zafar-auvee <rafath.auvee@gmail.com>
2023-03-05 03:42:21 +0600

    getting one user

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -10,3 +10,18 @@ module.exports.getAllUsers = async (req, res, next) => {
   }
 };
 
+module.exports.getOneUser = async (req, res, next) => {
+  try {
+    const id = parseInt(req.params.id);
+    const user = users.find((u) => u.id === id);
+    console.log(id);
+    console.log(user);
+    if (!user) {
+      return res.status(404).send("User not found");
+    } else {
+      res.status(200).json(user);
+    }
+  } catch (err) {
+    res.status(404).send(`Something Went Worng to Get One User Data!`);
+  }
+};
--- routes/v1/user.route.js
+++ routes/v1/user.route.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const userController = require("../../controllers/users.controller.js");
 
 router.route("/all").get(userController.getAllUsers);
-
+router.route("/:id").get(userController.getOneUser);
 
 module.exports = router;
